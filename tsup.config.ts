59344cf63ea6e8f4ab6cb9f82a6e091951fb414d
Damian Tarnawski <gthetarnav@gmail.com>
2023-01-20 15:19:07 +0100

    Use tsup-preset-solid for building

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,42 +1,14 @@
-import { defineConfig, Options } from 'tsup'
+import { defineConfig } from 'tsup-preset-solid'
 
-export default defineConfig(config => {
-  const common: Options = {
-    watch: !!config.watch,
-    target: 'esnext',
-    format: !!config.watch ? 'esm' : ['cjs', 'esm'],
-  }
-
-  const getEsbuildOption = (isDev: boolean): Options => ({
-    treeshake: !config.watch,
-    replaceNodeEnv: true,
-    esbuildOptions(options) {
-      options.define = {
-        ...options.define,
-        'process.env.NODE_ENV': isDev ? `"development"` : `"production"`,
-        'process.env.PROD': isDev ? '""' : '"1"',
-        'process.env.DEV': isDev ? '"1"' : '""',
-      }
-      return options
-    },
-  })
-
-  return [
-    {
-      ...common,
-      clean: true,
-      dts: 'src/index.ts',
-      entry: ['src/index.ts'],
-      ...getEsbuildOption(false),
-    },
-    {
-      ...common,
-      entry: { dev: 'src/index.ts' },
-      ...getEsbuildOption(true),
-    },
-    {
-      ...common,
-      entry: { server: 'src/server.ts' },
-    },
-  ]
-})
+export default defineConfig(
+  {
+    entry: 'src/index.ts',
+    serverEntry: 'src/server.ts',
+    devEntry: true,
+  },
+  {
+    // writePackageJson: true,
+    dropConsole: true,
+    cjs: true,
+  },
+)
